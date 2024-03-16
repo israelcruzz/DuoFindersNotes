9f7146c2f017b9e12bc6c34b03c6affc52550def
israelcruzz <israelcruz.contato@gmail.com>
2024-03-16 00:34:47 -0300

    feat: using cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express, { Response, Request, NextFunction } from "express";
 import { route } from "./routes";
 import { handleError } from "./utills/handleErrors";
 import path from "path";
+import cors from 'cors';
 
 const app = express();
 const port = 3030;
 
+app.use(cors())
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(express.json());
 
